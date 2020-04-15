910addffa193c56ce18e3d10f94d6e15be048a85
GustavoReis14 <gustavo.reis1402@gmail.com>
2020-04-15 20:24:30 -0300

    auxBinDec

--- para entregar/main.hs
+++ para entregar/main.hs
@@ -1,7 +1,15 @@
 import Data.Char
+import Data.List(elemIndices)
 
-isBin :: String -> Bool
-isBin cod = 
+--isBin :: String -> Bool
+
+
+isBin' :: String -> Bool
+isBin' cod = 
     let conv_cod = map digitToInt cod
         ver_cod = filter (>1) conv_cod
     in if ver_cod == [] then True else False 
+
+auxBinDec' :: [Int] -> Int
+auxBinDec' bin = sum (map (2^)  (elemIndices 1 (reverse bin)))
+          
